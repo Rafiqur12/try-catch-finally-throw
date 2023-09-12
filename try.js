223677e087669c31c8994ada6f1f3292f3b7f8ee
Ripon <ripon18922@gmail.com>
2023-09-12 10:05:23 +0600

    Try catch and finally is done

---
+++ try.js
@@ -0,0 +1,27 @@
+// Try catch and finally checking 
+
+
+// const tryButton = document.querySelector(".try-btn");
+// tryButton.addEventListener('click', function(){
+//     try{
+//         alert("Hi everyOne.");
+//         alert(x);
+//         alert("Bye everyOne.");
+//     }
+//     catch(err){
+//         alert("Inside catch error.")
+//        alert(err.name);
+//        alert(err.message);
+//     }
+//     finally{
+//         alert("Congratulations.");
+        
+//     }
+
+// });
+
+
+document.querySelector("#check-btn").addEventListener("click", function(){
+   var num = document.querySelector("#numTextField").value;
+    console.log(num);
+})
